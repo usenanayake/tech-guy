ffb52183bb2b9d0f1f07f1767e90fe9ce3bb1cec
Udara Senanayake <usenana94@gmail.com>
2018-12-02 19:48:48 +0530

    initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+// @Component({
+//   selector: 'form-field-overview-example',
+//   templateUrl: 'form-field-overview-example.html',
+//   styleUrls: ['form-field-overview-example.css'],
+// })
+
+
 export class AppComponent {
   title = 'Tech Guy';
 }
+// export class FormFieldOverviewExample {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,
     MDBBootstrapModule.forRoot()
     
   ],
@@ -19,3 +23,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// export class PizzaPartyAppModule { }
+
